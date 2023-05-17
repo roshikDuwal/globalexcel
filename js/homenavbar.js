d9a4c23d4052510a79b71ecb133e2eeb6dc2b923
Roshik <duwalroseek@gmail.com>
2023-05-17 22:25:51 +0545

    update error

--- js/homenavbar.js
+++ js/homenavbar.js
@@ -1,8 +1,11 @@
 const mobile_nav1=document.querySelector('.mobile-home-navbar-btn');
 const headerElem1 = document.querySelector('.heading');
-const body=document.querySelector('body');
+const body=document.querySelector('.body');
+const page = document.querySelector(".page")
 mobile_nav1.addEventListener('click',()=>{
     headerElem1.classList.toggle('active');
+    body.classList.toggle('scrollremove');
+    page.classList.toggle('blur');
 
 })
 
--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,11 @@
 const mobile_nav=document.querySelector('.mobile-navbar-btn');
 const headerElem = document.querySelector('.head');
+const page = document.querySelector(".page")
+const body= document.querySelector(".body")
 
 mobile_nav.addEventListener('click',()=>{
     headerElem.classList.toggle('active');
-  
+    page.classList.toggle('blur');
+    body.classList.toggle('scrollremove');
 })
 
